3938fb598d016e0fe2e80cd7c7cc01c474d2fe18
nchronas <nchronas@gmail.com>
2017-06-16 11:34:57 +0300

    UART and LED test for ARTY

--- software/asm_test/README.txt
+++ software/asm_test/README.txt
@@ -1,2 +1,8 @@
 riscv32-unknown-elf-as -march=IXpulpv2 test.asm
 riscv32-unknown-elf-objdump -d a.out
+
+riscv32-unknown-elf-objcopy -R .debug_frame -R .comment -R .stack -R .heapsram -R .heapscm -R .scmlock -R .rodata -R .eh_frame -R .shbss -R .data -R .bss -O binary --gap-fill=0 a.out a.bin
+riscv32-unknown-elf-objcopy -j .rodata -j .data -O binary --gap-fill=0 a.out a.dat
+riscv32-unknown-elf-objcopy -I binary -O verilog a.bin code.mem0
+riscv32-unknown-elf-objcopy -I binary -O verilog a.dat data.mem0
+../../romgen/bigromtest code.mem0 data.mem0 code.v data.v code.mem1 data.mem1
--- software/asm_test/test.asm
+++ software/asm_test/test.asm
@@ -1,34 +1,22 @@
 .text
-.global _start
-.org 0x20
-_start:
-nop
-addi x1, x1, 1
-nop
-nop
-nop
-addi x6, x0, 117 # 'u'
-lui x7, 0x200    # uart adress
-sw x6, mem1(x7)  # Tx byte
-nop
-nop
-nop
-lui x5, 0x100
-sw x1, mem1(x5)
-nop
-lw x2, mem1(x5)
-nop
-lw x3, mem1(x5)
-nop
-addi x3, x3, 2
-nop
-sw x3, mem2(x5)
-nop
-nop
-j _start
 
-.org 0x00100000
-.data
+.org 0x20
+_init:
+addi x1, x0, 1
+lui x2, 0x200    # uart adress
+lui x3, 0x700    # led adress
+_loop:
+
+addi x1, x1, 1
+
+sw x1, mem1(x2)  # Tx byte
+sw x1, mem1(x3)  # led byte
+
+
+j _loop
+
+.data 0x100000
+
             .align 3
-      mem1: .word 0
+      mem1: .long 0
       mem2: .word 1
